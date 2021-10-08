160e339c39ae9dcad7aa366507ae876a73050301
gensan-64 <genyuu6240@gmail.com>
2021-10-08 06:26:11 +0000

    会員のルーティングの設定

--- app/models/member.rb
+++ app/models/member.rb
@@ -12,4 +12,26 @@ class Member < ApplicationRecord
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
   validates :subject, length: { maximum: 10 }
+
+  # 自分がフォローされる（被フォロー）側の関係性
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  # 自分がフォローする（与フォロー）側の関係性
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  # 被フォロー関係を通じて参照→自分をフォローしている人
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+  # 与フォロー関係を通じて参照→自分がフォローしている人
+  has_many :followings, through: :relationships, source: :followed
+
+  def follow(member_id)
+    relationships.create(followed_id: member_id)
+  end
+
+  def unfollow(user_id)
+    relationships.find_by(followed_id: member_id).destroy
+  end
+
+  def following?(member)
+    followings.include?(member)
+  end
+
 end
