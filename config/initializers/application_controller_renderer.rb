8c9db2e7c823e19062cd7875c65b4b7dc5729e0a
gensan-64 <genyuu6240@gmail.com>
2021-10-13 01:45:52 +0000

    画像投稿機能の写真の挿入

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '15380505646fa3199635a2cae68b07719adb11ba55c9208ff08029698cb57f22d7489bd3db12e6d9eb31b84119d24c857c9ffec0828335de1e18a18b621cf152'
